2352462c5c94b6dfea7aa77a838f370d5bd94266
dontfuckmybrain <ilia.derum@gmail.com>
2025-05-18 21:22:04 +0300

    Add files via upload

---
+++ ScriptsManager.ps1
@@ -0,0 +1,42 @@
+﻿Write-Host "░██████╗░█████╗░██████╗░██╗██████╗░████████╗░██████╗  ███╗░░░███╗░█████╗░███╗░░██╗░█████╗░░██████╗░███████╗██████╗░" -ForegroundColor Blue
+Write-Host "██╔════╝██╔══██╗██╔══██╗██║██╔══██╗╚══██╔══╝██╔════╝  ████╗░████║██╔══██╗████╗░██║██╔══██╗██╔════╝░██╔════╝██╔══██╗" -ForegroundColor Blue
+Write-Host "╚█████╗░██║░░╚═╝██████╔╝██║██████╔╝░░░██║░░░╚█████╗░  ██╔████╔██║███████║██╔██╗██║███████║██║░░██╗░█████╗░░██████╔╝" -ForegroundColor Blue
+Write-Host "░╚═══██╗██║░░██╗██╔══██╗██║██╔═══╝░░░░██║░░░░╚═══██╗  ██║╚██╔╝██║██╔══██║██║╚████║██╔══██║██║░░╚██╗██╔══╝░░██╔══██╗" -ForegroundColor Blue
+Write-Host "██████╔╝╚█████╔╝██║░░██║██║██║░░░░░░░░██║░░░██████╔╝  ██║░╚═╝░██║██║░░██║██║░╚███║██║░░██║╚██████╔╝███████╗██║░░██║" -ForegroundColor Blue
+Write-Host "╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░╚═════╝░  ╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░╚══════╝╚═╝░░╚═╝" -ForegroundColor Blue
+
+
+$scriptList = @(
+    @{ Name = "HabibiModAnalyzer"; Url = "https://raw.githubusercontent.com/HadronCollision/PowershellScripts/refs/heads/main/HabibiModAnalyzer.ps1" },
+    @{ Name = "TaskSchedulerParser"; Url = "https://raw.githubusercontent.com/ObsessiveBf/Task-Scheduler-Parser/main/script.ps1" },
+    @{ Name = "RLBam"; Url = "https://raw.githubusercontent.com/dontfuckmybrain/myscripts/refs/heads/main/RLBam.ps1" },
+    @{ Name = "RedLotusPrefetchAnalyzer"; Url = "https://raw.githubusercontent.com/bacanoicua/Screenshare/main/RedLotusPrefetchIntegrityAnalyzer.ps1" },
+    @{ Name = "GetHarddiskVolumes"; Url = "https://raw.githubusercontent.com/dontfuckmybrain/myscripts/refs/heads/main/GetHarddiskVolumes.ps1" },
+    @{ Name = "RLSignature"; Url = "https://raw.githubusercontent.com/dontfuckmybrain/myscripts/refs/heads/main/RLSignature.ps1" },
+    @{ Name = "KernelLiveDumper"; Url = "https://raw.githubusercontent.com/dontfuckmybrain/myscripts/refs/heads/main/KernelLiveDumper.ps1" },
+    @{ Name = "MinecraftAlts"; Url = "https://raw.githubusercontent.com/dontfuckmybrain/myscripts/refs/heads/main/MinecraftAlts.ps1" }
+)
+
+
+Write-Host "`n`n`nДоступные скрипты:`n"
+for ($i = 0; $i -lt $scriptList.Count; $i++) {
+    Write-Host "$($i + 1)) $($scriptList[$i].Name)"
+}
+
+$input = Read-Host "`nВведите номера скриптов через запятую (например, 1,3,5)`n"
+$indices = $input -split "," | ForEach-Object { ($_ -as [int]) - 1 }
+
+foreach ($index in $indices) {
+    if ($index -ge 0 -and $index -lt $scriptList.Count) {
+        $script = $scriptList[$index]
+        Write-Host "`n[+] Загружаю и выполняю скрипт: $($script.Name)`n"
+        try {
+            $code = Invoke-RestMethod -Uri $script.Url
+            Invoke-Expression $code
+        } catch {
+            Write-Warning "Ошибка при загрузке или выполнении скрипта '$($script.Name)': $_"
+        }
+    } else {
+        Write-Warning "Неверный номер скрипта: $($index + 1)"
+    }
+}
