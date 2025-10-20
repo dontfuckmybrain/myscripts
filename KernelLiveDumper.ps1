8ed3c2507b9fc70d9e92eaccf89b493f71df43ed
dontfuckmybrain <ilia.derum@gmail.com>
2025-10-20 23:12:54 +0300

    Delete KernelLiveDumper.ps1

--- KernelLiveDumper.ps1
+++
@@ -1,4 +0,0 @@
-$StorageSubSystem = Get-CimInstance -ClassName MSFT_StorageSubSystem -Namespace Root\Microsoft\Windows\Storage
-Invoke-CimMethod -InputObject $StorageSubSystem -MethodName "GetDiagnosticInfo" -Arguments @{
-    DestinationPath = "C:\dmp"
-    IncludeLiveDump = $True}
